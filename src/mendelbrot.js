fcb19a4b39c112b8c2e6e8f16d8508d354818c16
Christian Torres Gonzalez <alu0101137902@ull.edu.es>
2020-04-08 12:57:12 +0100

    Probando curl en actions 5

--- src/mendelbrot.js
+++ src/mendelbrot.js
@@ -1,21 +1,21 @@
-/**
-  * @copyright Universidad de La Laguna
-  * Escuela Superior de Ingeniería y Tecnología
-  * Grado en Ingeniería Informática
-  * Asignatura: Programación de Aplicaciones Interactivas (PAI)
-  * Curso: 3º Itinerario 1
-  * Práctica 7 PAI - Mandelbrot. Numero complejos
-  * @author: Christian Torres Gonzalez alu0101137902@ull.edu.es
-  * @description: Programa que dado un lienzo de canvas, va a calcular, dada
-  * una coordenada X e Y, que dada un numro de iteraciones, para cada coordenada,
-  * y utilizando la función f(Zn) = Z(n-1)^2 + c, donde c es un numero complejo dado,
-  * y zn-1 es el resultado de elevar al cuadrado la funcion para valor Zn-1. Para poder
-  * llegar a un resultado deseado, debemos partir de la copndicion de que x e y, que son
-  * las coordenadas del punto del lienzo de canvas que estamos tratando.
-  * @since 28/03/2020
-  * @file Fichero de implementacion de ejercicios
-  * @version 1.0.0
-*/
+// /**
+//   * @copyright Universidad de La Laguna
+//   * Escuela Superior de Ingeniería y Tecnología
+//   * Grado en Ingeniería Informática
+//   * Asignatura: Programación de Aplicaciones Interactivas (PAI)
+//   * Curso: 3º Itinerario 1
+//   * Práctica 7 PAI - Mandelbrot. Numero complejos
+//   * @author: Christian Torres Gonzalez alu0101137902@ull.edu.es
+//   * @description: Programa que dado un lienzo de canvas, va a calcular, dada
+//   * una coordenada X e Y, que dada un numro de iteraciones, para cada coordenada,
+//   * y utilizando la función f(Zn) = Z(n-1)^2 + c, donde c es un numero complejo dado,
+//   * y zn-1 es el resultado de elevar al cuadrado la funcion para valor Zn-1. Para poder
+//   * llegar a un resultado deseado, debemos partir de la copndicion de que x e y, que son
+//   * las coordenadas del punto del lienzo de canvas que estamos tratando.
+//   * @since 28/03/2020
+//   * @file Fichero de implementacion de ejercicios
+//   * @version 1.0.0
+// */
 
 'use strict'
 
